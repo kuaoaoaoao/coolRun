039bded483d678f53d887bddee25544ba084639e
kuao <kuaoaoaoao@163.com>
2026-05-21 09:28:40 +0800

    Initial Commit

---
+++ coolRun/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  coolRun
+//
+//  Created by kuao on 2026/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ coolRun/coolRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  coolRunApp.swift
+//  coolRun
+//
+//  Created by kuao on 2026/5/21.
+//
+
+import SwiftUI
+
+@main
+struct coolRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
